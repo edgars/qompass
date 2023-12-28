64c66f7d482868b644bb25f2b485d8a5b2d81e16
edgars <edgar@skalena.com>
2023-12-28 13:59:00 -0300

    fixing stuff

--- src/routes/soap/index.js
+++ src/routes/soap/index.js
@@ -13,18 +13,27 @@ async function routes(app, options) {
     app.get("/:cepparam",cepSchema, async (request, response) => {
         const { cepparam } = request.params;
         console.log("Informed CEP: " + cepparam)
-        app.soapClient.consultaCEP({ cep:cepparam }, function (err, result) {
-            if (err) {
-                console.log("====>" + err);
-                response.header('X-Qompass-Completed', 'false');
-                return response.status(500).send( err)
-              
-            } else {
-                response.header('X-Qompass-Completed', 'true');
-                return response.status(200).send(result.consultaCEPResponse)
-            }
-          })  
+        try {
+            app.soapClient.consultaCEP({ cep:cepparam }, function (err, result) {
+                if (err) {
+                    console.log("====>" + err);
+                    response.header('X-Qompass-Completed', 'false');
+                    return response.status(500).send( err)
+                  
+                } else {
+                    response.header('X-Qompass-Completed', 'true');
+                    return response.status(200).send(result.consultaCEPResponse)
+                }
+              })  
+            
+        } catch (error) {
 
+            response.header('X-Qompass-Completed', 'false');
+            return response.status(500).send( error)
+            
+        }
+        
+         
     });
   }
   
